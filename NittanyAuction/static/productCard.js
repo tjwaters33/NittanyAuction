5e33a9af0f69e8c1f0a056512ac40b40a47730fa
nikita <nikita@vamrad.com>
2026-04-21 16:37:47 -0400

    few listing changes

--- NittanyAuction/static/productCard.js
+++ NittanyAuction/static/productCard.js
@@ -4,30 +4,30 @@ function makeProductCard(prod){
     card.className = 'card';
     card.style.width = '15rem';
     card.innerHTML = `
-        <div class="card-body p-3">
-        <h6 class="card-title fw-semibold mb-1">${prod.auction_title}</h6>
-        <p class="card-text text-muted small mb-0">${prod.product_description}</p>
-
-        <div class="col p-0">
-            <div class="row mt-2">
-                <div class="col-5 pr-1">
-                    <p class="fw-medium mb-0">$${prod.reserve_price.toFixed(2)}</p>
+        <div class="card-body p-3 d-flex flex-column">
+            <h6 class="card-title fw-semibold mb-1">${prod.auction_title}</h6>
+            <p class="card-text small mb-1">${prod.product_name}</p>
+            <p class="card-text text-muted small mb-0">${prod.product_description}</p>
+            <div class="col p-0">
+                <div class="row mt-2">
+                    <div class="col-5 pr-1">
+                        <p class="fw-medium mb-0">$${prod.reserve_price.toFixed(2)}</p>
+                    </div>
+                    <div class="col-4 pr-1 pl-0">
+                        <p class="mb-0">${prod.max_bids} bids</p>
+                    </div>
+                    <div class="col-3 pl-0">
+                        <p class="mb-0">${prod.quantity} qty</p>
+                    </div>
                 </div>
-                <div class="col-4 pr-1 pl-0">
-                    <p class="mb-0">${prod.max_bids} bids</p>
-                </div>
-                <div class="col-3 pl-0">
-                    <p class="mb-0">${prod.quantity} qty</p>
+                <div class="row mt-1 mb-2">
+                    <div class="col">
+                        <p class="small mb-0">${seller}</p>
+                    </div>
                 </div>
             </div>
-            <div class="row mt-1">
-                <div class="col">
-                    <p class="small mb-0">${seller}</p>
-                </div>
-            </div>
-        </div>
 
-        <a href="/listings/${prod.listing_id}" class="btn btn-primary mt-2 btn-sm w-100">View listing</a>
+            <a href="/listings/${prod.listing_id}" class="btn btn-primary mt-auto btn-sm w-100">View listing</a>
         </div>
     `;
     return card;
